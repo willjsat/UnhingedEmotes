43d135608ed16764b8b7cd380dc10c739fc3d0db
willjsat <angeelsalpha@gmail.com>
2026-02-03 21:47:48 +0000

    Add files via upload

---
+++ UnhingedTwitchEmotes.lua
@@ -0,0 +1,512 @@
+UnhingedTwitchEmotes = {
+	["Path"] = "Interface\\AddOns\\UnhingedTwitchEmotes\\Emotes\\Unhinged\\"
+};
+
+function UnhingedTwitchEmotes:init()
+	local emotes1 = {
+		["plenGa"] = {
+			["Name"] = "plenGa",
+			["Path"] = UnhingedTwitchEmotes.Path .. "plenGa.tga:28:28",
+		},
+		["syrup"] = {
+			["Name"] = "syrup",
+			["Path"] = UnhingedTwitchEmotes.Path .. "syrup.tga:28:28",
+		},
+		["Zased"] = {
+			["Name"] = "Zased",
+			["Path"] = UnhingedTwitchEmotes.Path .. "Zased.tga:28:28",
+		},
+		["GodEmperor"] = {
+			["Name"] = "GodEmperor",
+			["Path"] = UnhingedTwitchEmotes.Path .. "GodEmperor.tga:28:28",
+		},
+		["DRAINED"] = {
+			["Name"] = "DRAINED",
+			["Path"] = UnhingedTwitchEmotes.Path .. "DRAINED.tga:28:28",
+		},
+		["xdTree"] = {
+			["Name"] = "xdTree",
+			["Path"] = UnhingedTwitchEmotes.Path .. "xdTree.tga:28:28",
+		},
+		["Freg"] = {
+			["Name"] = "Freg",
+			["Path"] = UnhingedTwitchEmotes.Path .. "Freg.tga:28:28",
+		},
+		["shrekC"] = {
+			["Name"] = "shrekC",
+			["Path"] = UnhingedTwitchEmotes.Path .. "shrekC.tga:28:28",
+		},
+		["susCat"] = {
+			["Name"] = "susCat",
+			["Path"] = UnhingedTwitchEmotes.Path .. "susCat.tga:28:28",
+		},
+		["cockinspection"] = {
+			["Name"] = "cockinspection",
+			["Path"] = UnhingedTwitchEmotes.Path .. "cockinspection.tga:28:28",
+		},
+		["jermSmug"] = {
+			["Name"] = "jermSmug",
+			["Path"] = UnhingedTwitchEmotes.Path .. "jermSmug.tga:28:28",
+		},
+		["SadTree"] = {
+			["Name"] = "SadTree",
+			["Path"] = UnhingedTwitchEmotes.Path .. "SadTree.tga:28:28",
+		},
+		["xddCondi"] = {
+			["Name"] = "xddCondi",
+			["Path"] = UnhingedTwitchEmotes.Path .. "xddCondi.tga:28:56",
+		},
+		["NOW"] = {
+			["Name"] = "NOW",
+			["Path"] = UnhingedTwitchEmotes.Path .. "NOW.tga:28:28",
+		},
+		["NOTNOW"] = {
+			["Name"] = "NOTNOW",
+			["Path"] = UnhingedTwitchEmotes.Path .. "NOTNOW.tga:28:28",
+		},
+		["aaaa"] = {
+			["Name"] = "aaaa",
+			["Path"] = UnhingedTwitchEmotes.Path .. "aaaa.tga:28:28",
+		},
+		["PagMan"] = {
+			["Name"] = "PagMan",
+			["Path"] = UnhingedTwitchEmotes.Path .. "PagMan.tga:28:28",
+		},
+		["NAILS"] = {
+			["Name"] = "NAILS",
+			["Path"] = UnhingedTwitchEmotes.Path .. "NAILS.tga:28:28",
+		},
+		["SNAILS"] = {
+			["Name"] = "SNAILS",
+			["Path"] = UnhingedTwitchEmotes.Path .. "SNAILS.tga:28:56",
+		},
+		["Xased"] = {
+			["Name"] = "Xased",
+			["Path"] = UnhingedTwitchEmotes.Path .. "Xased.tga:28:28",
+		},
+		["ZasedDerail"] = {
+			["Name"] = "ZasedDerail",
+			["Path"] = UnhingedTwitchEmotes.Path .. "ZasedDerail.tga:28:28",
+		},
+		["plenMensius"] = {
+			["Name"] = "plenMensius",
+			["Path"] = UnhingedTwitchEmotes.Path .. "plenMensius.tga:28:28",
+		},
+		["BondageTree"] = {
+			["Name"] = "BondageTree",
+			["Path"] = UnhingedTwitchEmotes.Path .. "BondageTree.tga:28:28",
+		},
+		["treespair"] = {
+			["Name"] = "treespair",
+			["Path"] = UnhingedTwitchEmotes.Path .. "treespair.tga:28:28",
+		},
+		["zojjaGa"] = {
+			["Name"] = "zojjaGa",
+			["Path"] = UnhingedTwitchEmotes.Path .. "zojjaGa.tga:28:28",
+		},
+		["BOG"] = {
+			["Name"] = "BOG",
+			["Path"] = UnhingedTwitchEmotes.Path .. "BOG.tga:28:28",
+		},
+		["Fungus"] = {
+			["Name"] = "Fungus",
+			["Path"] = UnhingedTwitchEmotes.Path .. "Fungus.tga:28:28",
+		},
+		["Internetge"] = {
+			["Name"] = "Internetge",
+			["Path"] = UnhingedTwitchEmotes.Path .. "Internetge.tga:28:28",
+		},
+		["peepoSmile"] = {
+			["Name"] = "peepoSmile",
+			["Path"] = UnhingedTwitchEmotes.Path .. "peepoSmile.tga:28:28",
+		},
+		["PauseMan"] = {
+			["Name"] = "PauseMan",
+			["Path"] = UnhingedTwitchEmotes.Path .. "PauseMan.tga:28:28",
+		},
+	};
+
+	local emotes2 = {
+		["KEYS"] = {
+			["Name"] = "KEYS",
+			["Path"] = UnhingedTwitchEmotes.Path .. "KEYS.tga:28:28",
+		},
+		["LookUp"] = {
+			["Name"] = "LookUp",
+			["Path"] = UnhingedTwitchEmotes.Path .. "LookUp.tga:28:28",
+		},
+		["LETHIMCOOK"] = {
+			["Name"] = "LETHIMCOOK",
+			["Path"] = UnhingedTwitchEmotes.Path .. "LETHIMCOOK.tga:28:28",
+		},
+		["Hmmm"] = {
+			["Name"] = "Hmmm",
+			["Path"] = UnhingedTwitchEmotes.Path .. "Hmmm.tga:28:28",
+		},
+		["ZasedInfinite"] = {
+			["Name"] = "ZasedInfinite",
+			["Path"] = UnhingedTwitchEmotes.Path .. "ZasedInfinite.tga:28:28",
+		},
+		["ABOBA"] = {
+			["Name"] = "ABOBA",
+			["Path"] = UnhingedTwitchEmotes.Path .. "ABOBA.tga:28:56",
+		},
+		["o7"] = {
+			["Name"] = "o7",
+			["Path"] = UnhingedTwitchEmotes.Path .. "o7.tga:28:56",
+		},
+		["Cone"] = {
+			["Name"] = "Cone",
+			["Path"] = UnhingedTwitchEmotes.Path .. "Cone.tga:28:28",
+		},
+		["iAsk"] = {
+			["Name"] = "iAsk",
+			["Path"] = UnhingedTwitchEmotes.Path .. "iAsk.tga:28:28",
+		},
+		["smileW"] = {
+			["Name"] = "smileW",
+			["Path"] = UnhingedTwitchEmotes.Path .. "smileW.tga:28:28",
+		},
+		["YIKERS"] = {
+			["Name"] = "YIKERS",
+			["Path"] = UnhingedTwitchEmotes.Path .. "YIKERS.tga:28:28",
+		},
+		["TREEING"] = {
+			["Name"] = "TREEING",
+			["Path"] = UnhingedTwitchEmotes.Path .. "TREEING.tga:28:28",
+		},
+		["HOLY"] = {
+			["Name"] = "HOLY",
+			["Path"] = UnhingedTwitchEmotes.Path .. "HOLY.tga:28:28",
+		},
+		["Really"] = {
+			["Name"] = "Really",
+			["Path"] = UnhingedTwitchEmotes.Path .. "Really.tga:28:28",
+		},
+		["DRAUBED"] = {
+			["Name"] = "DRAUBED",
+			["Path"] = UnhingedTwitchEmotes.Path .. "DRAUBED.tga:28:28",
+		},
+		["CAUGHT"] = {
+			["Name"] = "CAUGHT",
+			["Path"] = UnhingedTwitchEmotes.Path .. "CAUGHT.tga:28:56",
+		},
+		["Sisyphus"] = {
+			["Name"] = "Sisyphus",
+			["Path"] = UnhingedTwitchEmotes.Path .. "Sisyphus.tga:28:28",
+		},
+		["ZAZA"] = {
+			["Name"] = "ZAZA",
+			["Path"] = UnhingedTwitchEmotes.Path .. "ZAZA.tga:28:28",
+		},
+		["bla"] = {
+			["Name"] = "bla",
+			["Path"] = UnhingedTwitchEmotes.Path .. "bla.tga:28:28",
+		},
+		["Nerdga"] = {
+			["Name"] = "Nerdga",
+			["Path"] = UnhingedTwitchEmotes.Path .. "Nerdga.tga:28:28",
+		},
+		["skully"] = {
+			["Name"] = "skully",
+			["Path"] = UnhingedTwitchEmotes.Path .. "skully.tga:28:28",
+		},
+		["xddTree"] = {
+			["Name"] = "xddTree",
+			["Path"] = UnhingedTwitchEmotes.Path .. "xddTree.tga:28:56",
+		},
+		["buh"] = {
+			["Name"] = "buh",
+			["Path"] = UnhingedTwitchEmotes.Path .. "buh.tga:28:56",
+		},
+		["Pointless"] = {
+			["Name"] = "Pointless",
+			["Path"] = UnhingedTwitchEmotes.Path .. "Pointless.tga:28:28",
+		},
+		["dawg"] = {
+			["Name"] = "dawg",
+			["Path"] = UnhingedTwitchEmotes.Path .. "dawg.tga:28:28",
+		},
+		["Angeels"] = {
+			["Name"] = "Angeels",
+			["Path"] = UnhingedTwitchEmotes.Path .. "Angeels.tga:28:56",
+		},
+		["buhlunt"] = {
+			["Name"] = "buhlunt",
+			["Path"] = UnhingedTwitchEmotes.Path .. "buhlunt.tga:28:56",
+		},
+		["SludgeHappy"] = {
+			["Name"] = "SludgeHappy",
+			["Path"] = UnhingedTwitchEmotes.Path .. "SludgeHappy.tga:28:56",
+		},
+		["HolyMoly"] = {
+			["Name"] = "HolyMoly",
+			["Path"] = UnhingedTwitchEmotes.Path .. "HolyMoly.tga:28:28",
+		},
+		["DonoWall"] = {
+			["Name"] = "DonoWall",
+			["Path"] = UnhingedTwitchEmotes.Path .. "DonoWall.tga:28:56",
+		},
+	};
+
+	local emotes3 = {
+		["MOLY"] = {
+			["Name"] = "MOLY",
+			["Path"] = UnhingedTwitchEmotes.Path .. "MOLY.tga:28:28",
+		},
+		["BOUGHT"] = {
+			["Name"] = "BOUGHT",
+			["Path"] = UnhingedTwitchEmotes.Path .. "BOUGHT.tga:28:56",
+		},
+		["JAILED"] = {
+			["Name"] = "JAILED",
+			["Path"] = UnhingedTwitchEmotes.Path .. "JAILED.tga:28:56",
+		},
+		["Plunge"] = {
+			["Name"] = "Plunge",
+			["Path"] = UnhingedTwitchEmotes.Path .. "Plunge.tga:28:56",
+		},
+		["CHILLS"] = {
+			["Name"] = "CHILLS",
+			["Path"] = UnhingedTwitchEmotes.Path .. "CHILLS.tga:28:28",
+		},
+		["zazabert"] = {
+			["Name"] = "zazabert",
+			["Path"] = UnhingedTwitchEmotes.Path .. "zazabert.tga:28:56",
+		},
+		["POG"] = {
+			["Name"] = "POG",
+			["Path"] = UnhingedTwitchEmotes.Path .. "POG.tga:28:56",
+		},
+		["itsok"] = {
+			["Name"] = "itsok",
+			["Path"] = UnhingedTwitchEmotes.Path .. "itsok.tga:28:28",
+		},
+		["tuh"] = {
+			["Name"] = "tuh",
+			["Path"] = UnhingedTwitchEmotes.Path .. "tuh.tga:28:28",
+		},
+		["zuh"] = {
+			["Name"] = "zuh",
+			["Path"] = UnhingedTwitchEmotes.Path .. "zuh.tga:28:28",
+		},
+		["yuh"] = {
+			["Name"] = "yuh",
+			["Path"] = UnhingedTwitchEmotes.Path .. "yuh.tga:28:56",
+		},
+		["ta"] = {
+			["Name"] = "ta",
+			["Path"] = UnhingedTwitchEmotes.Path .. "ta.tga:28:28",
+		},
+		["HAH"] = {
+			["Name"] = "HAH",
+			["Path"] = UnhingedTwitchEmotes.Path .. "HAH.tga:28:56",
+		},
+		["joever"] = {
+			["Name"] = "joever",
+			["Path"] = UnhingedTwitchEmotes.Path .. "joever.tga:28:28",
+		},
+		["buhg"] = {
+			["Name"] = "buhg",
+			["Path"] = UnhingedTwitchEmotes.Path .. "buhg.tga:28:28",
+		},
+		["coomer"] = {
+			["Name"] = "coomer",
+			["Path"] = UnhingedTwitchEmotes.Path .. "coomer.tga:28:28",
+		},
+		["CINEMA"] = {
+			["Name"] = "CINEMA",
+			["Path"] = UnhingedTwitchEmotes.Path .. "CINEMA.tga:28:56",
+		},
+		["COOKED"] = {
+			["Name"] = "COOKED",
+			["Path"] = UnhingedTwitchEmotes.Path .. "COOKED.tga:28:28",
+		},
+		["magrest"] = {
+			["Name"] = "magrest",
+			["Path"] = UnhingedTwitchEmotes.Path .. "magrest.tga:28:28",
+		},
+		["delulu"] = {
+			["Name"] = "delulu",
+			["Path"] = UnhingedTwitchEmotes.Path .. "delulu.tga:28:28",
+		},
+		["CatChest"] = {
+			["Name"] = "CatChest",
+			["Path"] = UnhingedTwitchEmotes.Path .. "CatChest.tga:28:28",
+		},
+		["aga"] = {
+			["Name"] = "aga",
+			["Path"] = UnhingedTwitchEmotes.Path .. "aga.tga:28:28",
+		},
+		["Bearge"] = {
+			["Name"] = "Bearge",
+			["Path"] = UnhingedTwitchEmotes.Path .. "Bearge.tga:28:28",
+		},
+		["garf"] = {
+			["Name"] = "garf",
+			["Path"] = UnhingedTwitchEmotes.Path .. "garf.tga:28:28",
+		},
+		["bro"] = {
+			["Name"] = "bro",
+			["Path"] = UnhingedTwitchEmotes.Path .. "bro.tga:28:28",
+		},
+		["Dentge"] = {
+			["Name"] = "Dentge",
+			["Path"] = UnhingedTwitchEmotes.Path .. "Dentge.tga:28:56",
+		},
+		["hesright"] = {
+			["Name"] = "hesright",
+			["Path"] = UnhingedTwitchEmotes.Path .. "hesright.tga:28:56",
+		},
+		["Baseg"] = {
+			["Name"] = "Baseg",
+			["Path"] = UnhingedTwitchEmotes.Path .. "Baseg.tga:28:28",
+		},
+		["cactus"] = {
+			["Name"] = "cactus",
+			["Path"] = UnhingedTwitchEmotes.Path .. "cactus.tga:28:28",
+		},
+		["todd"] = {
+			["Name"] = "todd",
+			["Path"] = UnhingedTwitchEmotes.Path .. "todd.tga:28:28",
+		},
+	};
+
+	local emotes4 = {
+		["toddGun"] = {
+			["Name"] = "toddGun",
+			["Path"] = UnhingedTwitchEmotes.Path .. "toddGun.tga:28:28",
+		},
+		["Noirge"] = {
+			["Name"] = "Noirge",
+			["Path"] = UnhingedTwitchEmotes.Path .. "Noirge.tga:28:56",
+		},
+		["frfr"] = {
+			["Name"] = "frfr",
+			["Path"] = UnhingedTwitchEmotes.Path .. "frfr.tga:28:28",
+		},
+		["Okayeg"] = {
+			["Name"] = "Okayeg",
+			["Path"] = UnhingedTwitchEmotes.Path .. "Okayeg.tga:28:28",
+		},
+		["aug"] = {
+			["Name"] = "aug",
+			["Path"] = UnhingedTwitchEmotes.Path .. "aug.tga:28:28",
+		},
+		["gnn"] = {
+			["Name"] = "gnn",
+			["Path"] = UnhingedTwitchEmotes.Path .. "gnn.tga:28:28",
+		},
+		["tomfoolery"] = {
+			["Name"] = "tomfoolery",
+			["Path"] = UnhingedTwitchEmotes.Path .. "tomfoolery.tga:28:28",
+		},
+		["Dogege"] = {
+			["Name"] = "Dogege",
+			["Path"] = UnhingedTwitchEmotes.Path .. "Dogege.tga:28:56",
+		},
+		["awakebutatwhatcost"] = {
+			["Name"] = "awakebutatwhatcost",
+			["Path"] = UnhingedTwitchEmotes.Path .. "awakebutatwhatcost.tga:28:28",
+		},
+		["PoroSad"] = {
+			["Name"] = "PoroSad",
+			["Path"] = UnhingedTwitchEmotes.Path .. "PoroSad.tga:28:28",
+		},
+		["grok"] = {
+			["Name"] = "grok",
+			["Path"] = UnhingedTwitchEmotes.Path .. "grok.tga:28:56",
+		},
+		["slop"] = {
+			["Name"] = "slop",
+			["Path"] = UnhingedTwitchEmotes.Path .. "slop.tga:28:28",
+		},
+		["erm"] = {
+			["Name"] = "erm",
+			["Path"] = UnhingedTwitchEmotes.Path .. "erm.tga:28:28",
+		},
+		["ALOO"] = {
+			["Name"] = "ALOO",
+			["Path"] = UnhingedTwitchEmotes.Path .. "ALOO.tga:28:28",
+		},
+		["BRUH"] = {
+			["Name"] = "BRUH",
+			["Path"] = UnhingedTwitchEmotes.Path .. "BRUH.tga:28:28",
+		},
+		["nO"] = {
+			["Name"] = "nO",
+			["Path"] = UnhingedTwitchEmotes.Path .. "nO.tga:28:28",
+		},
+		["PogU"] = {
+			["Name"] = "PogU",
+			["Path"] = UnhingedTwitchEmotes.Path .. "PogU.tga:28:28",
+		},
+		["WHOLETHIMCOOK"] = {
+			["Name"] = "WHOLETHIMCOOK",
+			["Path"] = UnhingedTwitchEmotes.Path .. "WHOLETHIMCOOK.tga:28:28",
+		},
+		["Saj"] = {
+			["Name"] = "Saj",
+			["Path"] = UnhingedTwitchEmotes.Path .. "Saj.tga:28:56",
+		},
+		["Streaming"] = {
+			["Name"] = "Streaming",
+			["Path"] = UnhingedTwitchEmotes.Path .. "Streaming.tga:28:28",
+		},
+		["auh"] = {
+			["Name"] = "auh",
+			["Path"] = UnhingedTwitchEmotes.Path .. "auh.tga:28:28",
+		},
+	};
+
+	local emoteIDs = {};
+
+	for id, emoteInfo in pairs(emotes1) do
+		table.insert(emoteIDs, id);
+
+		TwitchEmotes:AddEmote(id, emoteInfo.Name, emoteInfo.Path);
+	end
+
+	TwitchEmotes:AddCategory("Unhinged 1", emoteIDs);
+
+	emoteIDs = {};
+
+	for id, emoteInfo in pairs(emotes2) do
+		table.insert(emoteIDs, id);
+
+		TwitchEmotes:AddEmote(id, emoteInfo.Name, emoteInfo.Path);
+	end
+
+	TwitchEmotes:AddCategory("Unhinged 2", emoteIDs);
+
+	emoteIDs = {};
+
+	for id, emoteInfo in pairs(emotes3) do
+		table.insert(emoteIDs, id);
+
+		TwitchEmotes:AddEmote(id, emoteInfo.Name, emoteInfo.Path);
+	end
+
+	TwitchEmotes:AddCategory("Unhinged 3", emoteIDs);
+
+	emoteIDs = {};
+
+	for id, emoteInfo in pairs(emotes4) do
+		table.insert(emoteIDs, id);
+
+		TwitchEmotes:AddEmote(id, emoteInfo.Name, emoteInfo.Path);
+	end
+
+	TwitchEmotes:AddCategory("Unhinged 4", emoteIDs);
+end
+
+local frame = CreateFrame("Frame", "UnhingedTwitchEmotesFrame");
+frame:RegisterEvent("ADDON_LOADED");
+frame:SetScript(
+	"OnEvent",
+	function (frame, event, ...)
+		if (event == "ADDON_LOADED" and ... == "UnhingedTwitchEmotes") then
+			UnhingedTwitchEmotes:init();
+		end
+	end
+);
